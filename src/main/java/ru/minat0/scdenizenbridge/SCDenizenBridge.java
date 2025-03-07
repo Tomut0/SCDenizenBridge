8d3d5a167317b10a680f5d19c54ea324efa39d55
Tomut0 <minat0_@outlook.com>
2025-03-07 21:40:46 +0300

    fix: Depenizen#loadBridge breaking change

--- src/main/java/ru/minat0/scdenizenbridge/SCDenizenBridge.java
+++ src/main/java/ru/minat0/scdenizenbridge/SCDenizenBridge.java
@@ -1,8 +1,13 @@
 package ru.minat0.scdenizenbridge;
 
+import com.denizenscript.depenizen.bukkit.Bridge;
 import com.denizenscript.depenizen.bukkit.Depenizen;
 import net.sacredlabyrinth.phaed.simpleclans.SimpleClans;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Method;
+import java.util.function.Supplier;
+import java.util.logging.Level;
 
 public final class SCDenizenBridge extends JavaPlugin {
 
@@ -17,13 +22,42 @@ public final class SCDenizenBridge extends JavaPlugin {
         return instance;
     }
 
-    @SuppressWarnings("Convert2MethodRef")
     @Override
     public void onEnable() {
         // Plugin startup logic
         instance = this;
         scPlugin = SimpleClans.getInstance();
-        Depenizen.instance.registerBridge("SimpleClans", () -> new SCBridge());
-        Depenizen.instance.loadBridge("SimpleClans", () -> new SCBridge());
+        Depenizen.instance.registerBridge("SimpleClans", getSupplier());
+
+        String DepVersion = Depenizen.instance.getDescription().getVersion();
+        String[] semVer = DepVersion.split("\\.");
+
+        if (semVer.length > 1 && semVer.length <= 3) {
+            int major = Integer.parseInt(semVer[0]);
+            int minor = Integer.parseInt(semVer[1]);
+            if (major == 2 && minor >= 1) {
+                Depenizen.instance.loadBridge("SimpleClans", new Depenizen.BridgeData("ru.minat0.scdenizenbridge.SCDenizenBridge", getSupplier()));
+            } else {
+                loadOldDepenizen();
+            }
+        } else {
+            instance.getLogger().severe("Unrecognized version: " + DepVersion);
+        }
+    }
+
+    private static void loadOldDepenizen() {
+        try {
+            // Get the loadBridge method
+            Method loadBridge = Depenizen.class.getDeclaredMethod("loadBridge", String.class, Supplier.class);
+            loadBridge.setAccessible(true);
+            // Invoke the method on the Depenizen instance
+            loadBridge.invoke(Depenizen.instance, "SimpleClans", getSupplier());
+        } catch (ReflectiveOperationException ex) {
+            instance.getLogger().log(Level.SEVERE, "Failed to load Depenizen <= 2.0.0", ex);
+        }
+    }
+
+    private static Supplier<Bridge> getSupplier() {
+        return SCBridge::new;
     }
 }
